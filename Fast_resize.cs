94673fa767d7bbd23d235269e86cf22ab69359d8
sand9985 <sand9985@users.noreply.github.com>
2023-11-18 18:33:24 +0800

    Update Fast_resize.cs
    update

--- Fast_resize.cs
+++ Fast_resize.cs
@@ -5,36 +5,40 @@ using System.Threading.Tasks;
 
 public class Fast_resize
 {
-    public enum Color_type
+    public Fast_resize():this(false)  {}
+    public Fast_resize(bool source_inverse)
     {
-        RGB,BGR
+        this.Source_inverse = source_inverse;
     }
-    private Color_type source_color_type = Color_type.BGR;
-    private int r_offset=2,g_offfset=1,b_offfset=1;
-    public Color_type Source_color_type
-    {
-        get { return source_color_type;}
+    private int r_offset = 2, g_offfset = 1, b_offfset = 1;
 
+    private bool color_inverse = false;
+    public bool Source_inverse
+    {
+        get   {  return color_inverse;  }
         set
         {
-            if(value == Color_type.RGB)
+            this.color_inverse = value;
+            if (this.color_inverse == true)
             {
-                r_offset = 0; g_offfset=1; b_offfset=2;
+                r_offset = 0;
+                g_offfset = 1;
+                b_offfset = 2;
             }
             else
             {
-                r_offset = 2; g_offfset = 1; b_offfset = 0;
+                r_offset = 2;
+                g_offfset = 1;
+                b_offfset = 0;
+
             }
-            source_color_type = value;
         }
-
-
     }
-  
 
     public  unsafe Image<Rgb, float> resize_nearest(Mat originImage, Rectangle roi, int w, int h)
     {
 
+  
         Image<Rgb, float> new_mat = new Image<Rgb, float>(w, h);
         byte* data = (byte*)originImage.DataPointer;
         int stride = originImage.Width;
@@ -44,28 +48,93 @@ public class Fast_resize
         float scale_y = roi.Height / (float)h;
         int max_len = originImage.Width * originImage.Height * 3;
 
-        Parallel.For(0, h, i =>
-        {
-            int x, y;
-            int pos;
-            y = (int)(i * scale_y) + orign_y;
-            for (int j = 0; j < w; j++)
+     
+            Parallel.For(0, h, i =>
             {
+                int x, y;
+                int pos;
+                y = (int)(i * scale_y) + orign_y;
+                for (int j = 0; j < w; j++)
+                {
+                    x = (int)(j * scale_x) + orign_x;
+                    pos = (int)(y * stride + x) * 3;
+                    if (pos < 0 || pos >= max_len) continue;
+                    new_mat.Data[i, j, 0] = data[pos + r_offset];
+                    new_mat.Data[i, j, 1] = data[pos + g_offfset];
+                    new_mat.Data[i, j, 2] = data[pos + b_offfset];
+                }
+            });
 
-                x = (int)(j * scale_x) + orign_x;
-
-                pos = (int)(y * stride + x) * 3;
-
-                if (pos < 0 || pos >= max_len) continue;
-
-                new_mat.Data[i, j, 0] = data[pos + r_offset];
-                new_mat.Data[i, j, 1] = data[pos + g_offfset];
-                new_mat.Data[i, j, 2] = data[pos + b_offfset];
-            }
-        });
+        
 
         return new_mat;
     }
 
+
+
+    public unsafe Image<Rgb, float> resize_bilinear(Mat originImage, Rectangle roi, int w, int h)
+    {
+
+        Image<Rgb, float> new_mat = new Image<Rgb, float>(w, h);
+        byte* data = (byte*)originImage.DataPointer;
+        int stride = originImage.Width * 3;
+        int orign_x = roi.X;
+        int orign_y = roi.Y;
+
+        float scale_x = roi.Width / (float)w;
+        float scale_y = roi.Height / (float)h;
+        int max_len = originImage.Width * originImage.Height * 3;
+
+
+        Parallel.For(0, h, (i) =>
+        {
+            float x, y;
+            float a, b;
+            int pos, pos2, pos3, pos4;
+            float sub_a, sub_b;
+
+            y = (i * scale_y) + orign_y;
+            b = y - (int)y;
+            y = (int)y;
+            y *= stride;
+            sub_b = 1 - b;
+            y += 3 * orign_x;
+
+            for (int j = 0; j < w; j++)
+            {
+
+                x = (j * scale_x);
+                a = x - (int)x;
+                x = (int)x;
+                pos = (int)(y + 3 * x);
+
+                if (pos < 0 || pos >= max_len) continue;
+
+                pos2 = pos + stride;
+                pos3 = pos + 3;
+                pos4 = pos + stride + 3;
+                sub_a = 1 - a;
+
+                new_mat.Data[i, j, 0] = (sub_a * sub_b * data[pos + r_offset] +
+               sub_a * b * data[pos2 + r_offset] +
+                   a * sub_b * data[pos3 + r_offset] +
+                   a * b * data[pos4 + r_offset]);
+
+                new_mat.Data[i, j, 1] = (sub_a * sub_b * data[pos + g_offfset] +
+               sub_a * b * data[pos2 + g_offfset] +
+                   a * sub_b * data[pos3 + g_offfset] +
+                   a * b * data[pos4 + g_offfset]);
+
+                new_mat.Data[i, j, 2] = (sub_a * sub_b * data[pos+b_offfset] +
+                    sub_a * b * data[pos2+ b_offfset] +
+                     a * sub_b * data[pos3+ b_offfset] +
+                     a * b * data[pos4+ b_offfset]) ;                           
+            }
+        });
+
+
+
+        return new_mat;
+    }
 }
 
